54a83cf3151c6e1d6e161779a468f78f829c0cdc
LucasFigari <figarilucas4@gmail.com>
2026-05-13 21:48:02 -0300

    Se agregró la verificación con la bd para dni y email (datos únicos). Se realizó los metodos correspondientes y conexiones entre controlador, rutas y el registrarpersonl.js

--- backend/src/controllers/PersonalController.js
+++ backend/src/controllers/PersonalController.js
@@ -3,6 +3,7 @@ import AppDataSource from "../config/DbConfig.js"
 import PersonalSchema from "../schema/PersonalSchema.js"
 
 export const registrarPersonal = async (req, res) => {
+
     const { nombre, apellido, fechaNac, dni, email, telefono, password, rol } = req.body
 
     const repo = AppDataSource.getRepository(PersonalSchema)
@@ -11,4 +12,37 @@ export const registrarPersonal = async (req, res) => {
     await repo.save(nuevoPersonal)
 
     res.json({ ok: true, mensaje: "Personal registrado correctamente" })
+
+}
+
+export const validarDniUnico = async (req, res) => {
+
+    const { dni } = req.params
+
+    const repo = AppDataSource.getRepository(PersonalSchema)
+
+    const personalExistente = await repo.findOne({ where: { dni: dni }})
+
+    if(personalExistente){
+        res.json({ existe: true })
+    }
+    else{
+        res.json({ existe: false })
+    }
+}
+
+export const validarCorreoUnico = async (req, res) => {
+
+    const { email } = req.params
+
+    const repo = AppDataSource.getRepository(PersonalSchema)
+
+    const personalExistente = await repo.findOne({ where: { email: email }})
+
+    if(personalExistente){
+        res.json({ existe: true })
+    }
+    else{
+        res.json({ existe: false })
+    }
 }
--- backend/src/routes/PersonalRoutes.js
+++ backend/src/routes/PersonalRoutes.js
@@ -1,9 +1,13 @@
 // src/routes/PersonalRoutes.js
 import { Router } from "express"
-import { registrarPersonal } from "../controllers/PersonalController.js"
+import { registrarPersonal, validarDniUnico, validarCorreoUnico } from "../controllers/PersonalController.js"
 
 const router = Router()
 
 router.post("/registrar", registrarPersonal)
+
+router.get("/verificar-dni:dni", validarDniUnico)
+
+router.get("/verificar-email:email", validarCorreoUnico)
 
 export default router
